6278966cbf5566aaff0f68aa9cb71dddcdb26433
LUIZ ALBERTO SANTANNA FILHO <luizsantanna@tttcorporate.com>
2026-01-02 21:10:39 +0000

    File changes

--- src/pages/InvestorDashboard.jsx
+++ src/pages/InvestorDashboard.jsx
@@ -474,8 +474,8 @@ export default function InvestorDashboard() {
                         </div>
                       </div>
 
-                    {/* Data de Retorno */}
-                    <div className="bg-green-600/10 rounded-lg p-4 border border-green-500/30 flex items-center justify-between">
+                      {/* Data de Retorno */}
+                      <div className="bg-green-600/10 rounded-lg p-4 border border-green-500/30 flex items-center justify-between">
                       <div className="flex items-center gap-3">
                         <Clock className="w-5 h-5 text-green-400" />
                         <div>
