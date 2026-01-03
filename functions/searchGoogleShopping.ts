a8de54b538ecb7909456eb21772f9ad97605d149
LUIZ ALBERTO SANTANNA FILHO <luizsantanna@tttcorporate.com>
2026-01-03 02:17:53 +0000

    File changes

--- functions/searchGoogleShopping.ts
+++ functions/searchGoogleShopping.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 
 // 🔥 FUNÇÃO CRÍTICA: Remove "Leilão NoZap" GARANTIDO
 function simplifyProductTitle(title) {
