518e5c3e703e61d4e0cb144f964324c8a0b2e8a6
LUIZ ALBERTO SANTANNA FILHO <luizsantanna@tttcorporate.com>
2026-01-04 16:40:31 +0000

    File changes

--- src/pages/Licensing.jsx
+++ src/pages/Licensing.jsx
@@ -956,7 +956,7 @@ const DashboardContent = ({ user, isAdmin }) => {
                 </Button>
                 <Button
                   onClick={() => setShowWithdrawalModal(true)}
-                  className="bg-green-600 hover:bg-green-700"
+                  className="bg-gray-700 hover:bg-gray-600 text-white"
                 >
                   <Wallet className="w-4 h-4 mr-2" />
                   Sacar Dinheiro
