bb73c1f8f920eb10cb408fc279834949f1ab0186
LUIZ ALBERTO SANTANNA FILHO <luizsantanna@tttcorporate.com>
2026-01-10 23:31:56 +0000

    File changes

--- src/components/catalog/CatalogProductCard.jsx
+++ src/components/catalog/CatalogProductCard.jsx
@@ -298,6 +298,23 @@ function CatalogProductCard({ product, currentUser }) {
         </div>
       </CardContent>
     </Card>
+
+    {/* Modal Comparai */}
+    {showComparai && (
+      <ComparaiModal 
+        auction={{
+          id: product.id,
+          title: product.description,
+          current_price: product.price_catalog,
+          starting_price: product.price_catalog,
+          image_urls: product.image_urls,
+          category: product.category
+        }}
+        isProduct={true}
+        onClose={() => setShowComparai(false)} 
+      />
+    )}
+    </>
   );
 }
 
