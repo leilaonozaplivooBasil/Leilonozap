578eb0c3ea192ff9c94ce8decf10bb91282d061c
LUIZ ALBERTO SANTANNA FILHO <luizsantanna@tttcorporate.com>
2026-01-11 15:11:33 +0000

    File changes

--- src/pages/CreateAuction.jsx
+++ src/pages/CreateAuction.jsx
@@ -127,7 +127,7 @@ export default function CreateAuction() {
   const handleValidationCancel = () => {
     setShowValidationModal(false);
     setValidationData(null);
-    setProductName("");
+    setAuctionUrl("");
   };
 
 
