c616fce15a9e8cfd21c150dfea396af4377103b6
LUIZ ALBERTO SANTANNA FILHO <luizsantanna@tttcorporate.com>
2026-01-14 02:40:26 +0000

    File changes

--- src/components/pdv/DailyRanking.jsx
+++ src/components/pdv/DailyRanking.jsx
@@ -189,7 +189,7 @@ export default function DailyRanking({ allSales }) {
             <span className="text-green-400 font-semibold whitespace-nowrap">Total R$ {fmt(dayTotal || 0)}</span>
           </div>
         </div>
-        <img src={NoZapLogo} alt="Leilão NoZap" crossOrigin="anonymous" className="h-9 sm:h-10 md:h-12 object-contain rounded max-w-[80px]" />
+        <img src={NoZapLogo} alt="Leilão NoZap" crossOrigin="anonymous" className="h-9 sm:h-10 md:h-12 object-contain rounded max-w-[80px]" onError={(e)=>{e.currentTarget.style.display='none';}} />
       </div>
 
       {/* Lista Top 10 */}
