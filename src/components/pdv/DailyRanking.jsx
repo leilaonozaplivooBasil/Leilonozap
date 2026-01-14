953ba13d12915cc617e9cfafd8541b9fbf066e60
LUIZ ALBERTO SANTANNA FILHO <luizsantanna@tttcorporate.com>
2026-01-14 02:21:42 +0000

    File changes

--- src/components/pdv/DailyRanking.jsx
+++ src/components/pdv/DailyRanking.jsx
@@ -208,8 +208,9 @@ export default function DailyRanking({ allSales }) {
       </div>
 
       </div>
-      {/* Ações */}
-      <div className="mt-4 flex items-center justify-end">
+    </div>
+    {/* Ações */}
+    <div className="mt-4 flex items-center justify-end">
         <Button onClick={handleShare} disabled={sharing} className="bg-green-600 hover:bg-green-700 disabled:opacity-50">
           {sharing ? (
             <>
