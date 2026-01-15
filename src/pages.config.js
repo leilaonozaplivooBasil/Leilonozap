91f885f3ea4f3a926de3fbd8c01589c16924406b
LUIZ ALBERTO SANTANNA FILHO <luizsantanna@tttcorporate.com>
2026-01-15 04:11:55 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -15,7 +15,6 @@ import CatalogProductDetails from './pages/CatalogProductDetails';
 import Checkout from './pages/Checkout';
 import CreateAuction from './pages/CreateAuction';
 import CreateAuctionV2 from './pages/CreateAuctionV2';
-import CreateLuxuryAuction from './pages/CreateLuxuryAuction';
 import CustomerDetails from './pages/CustomerDetails';
 import EditAuction from './pages/EditAuction';
 import EditCatalogProduct from './pages/EditCatalogProduct';
@@ -32,7 +31,6 @@ import LiveShopControl from './pages/LiveShopControl';
 import LiveShopControlNoZap from './pages/LiveShopControlNoZap';
 import LiveShopNoZap from './pages/LiveShopNoZap';
 import LojistaDashboard from './pages/LojistaDashboard';
-import LuxuryAccessManager from './pages/LuxuryAccessManager';
 import LuxuryCollection from './pages/LuxuryCollection';
 import MemoryBackup from './pages/MemoryBackup';
 import MyWinnings from './pages/MyWinnings';
@@ -58,6 +56,8 @@ import SystemTest from './pages/SystemTest';
 import TesteLeilao from './pages/TesteLeilao';
 import TransactionHistory from './pages/TransactionHistory';
 import WalletHistory from './pages/WalletHistory';
+import CreateLuxuryAuction from './pages/CreateLuxuryAuction';
+import LuxuryAccessManager from './pages/LuxuryAccessManager';
 import __Layout from './Layout.jsx';
 
 
@@ -79,7 +79,6 @@ export const PAGES = {
     "Checkout": Checkout,
     "CreateAuction": CreateAuction,
     "CreateAuctionV2": CreateAuctionV2,
-    "CreateLuxuryAuction": CreateLuxuryAuction,
     "CustomerDetails": CustomerDetails,
     "EditAuction": EditAuction,
     "EditCatalogProduct": EditCatalogProduct,
@@ -96,7 +95,6 @@ export const PAGES = {
     "LiveShopControlNoZap": LiveShopControlNoZap,
     "LiveShopNoZap": LiveShopNoZap,
     "LojistaDashboard": LojistaDashboard,
-    "LuxuryAccessManager": LuxuryAccessManager,
     "LuxuryCollection": LuxuryCollection,
     "MemoryBackup": MemoryBackup,
     "MyWinnings": MyWinnings,
@@ -122,6 +120,8 @@ export const PAGES = {
     "TesteLeilao": TesteLeilao,
     "TransactionHistory": TransactionHistory,
     "WalletHistory": WalletHistory,
+    "CreateLuxuryAuction": CreateLuxuryAuction,
+    "LuxuryAccessManager": LuxuryAccessManager,
 }
 
 export const pagesConfig = {
