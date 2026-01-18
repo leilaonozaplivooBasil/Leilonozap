c351d30b11516e4c16ca49820f7cf2a863c181d2
LUIZ ALBERTO SANTANNA FILHO <luizsantanna@tttcorporate.com>
2026-01-18 21:09:20 +0000

    File changes

--- src/pages/Licensing.jsx
+++ src/pages/Licensing.jsx
@@ -35,7 +35,6 @@ import EarningsSimulator from '../components/licensing/EarningsSimulator';
 import JourneyAnimation from '../components/licensing/JourneyAnimation';
 import DraggableUserHierarchy from '../components/licensing/DraggableUserHierarchy';
 import HierarchyTreeView from '../components/licensing/HierarchyTreeView';
-import EditableOrganigramTree from '../components/licensing/EditableOrganigramTree';
 import CatalogProductCard from '../components/catalog/CatalogProductCard';
 import RotatingBanner from '../components/banner/RotatingBanner';
 import CatalogHome from '../components/lojista/CatalogHome';
