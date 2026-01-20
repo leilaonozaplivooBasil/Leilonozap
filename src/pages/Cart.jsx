f649ef29f856cae303ab09f4f6c371d18c450a9d
LUIZ ALBERTO SANTANNA FILHO <luizsantanna@tttcorporate.com>
2026-01-20 01:43:20 +0000

    File changes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -270,10 +270,20 @@ export default function Cart() {
             {/* Botão de checkout */}
             <Button
               onClick={handleCheckout}
-              className="w-full bg-green-600 hover:bg-green-700 h-14 text-lg font-semibold"
+              disabled={isProcessing}
+              className="w-full bg-green-600 hover:bg-green-700 h-14 text-lg font-semibold disabled:opacity-70"
             >
-              <ShoppingBag className="w-5 h-5 mr-2" />
-              Finalizar Compra
+              {isProcessing ? (
+                <>
+                  <Loader2 className="w-5 h-5 mr-2 animate-spin" />
+                  Processando...
+                </>
+              ) : (
+                <>
+                  <ShoppingBag className="w-5 h-5 mr-2" />
+                  Finalizar Compra
+                </>
+              )}
             </Button>
           </div>
         )}
