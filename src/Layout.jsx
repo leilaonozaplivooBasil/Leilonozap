2d79f232aca99144ccb007ab34de3bd46aa9e2ea
LUIZ ALBERTO SANTANNA FILHO <luizsantanna@tttcorporate.com>
2026-01-20 19:41:26 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -460,6 +460,7 @@ export default function Layout({ children, currentPageName }) {
     { title: "💰 PDV", pageName: "PDV" },
     { title: "📊 CRM", pageName: "CRM" },
     { title: "🏪 Registrar Lojista", pageName: "StoreRegistration" },
+    { title: "👥 Registrar Licenciados", pageName: "LicenseeManager" },
     { title: "👥 Influenciadores", pageName: "InfluencersDashboard" },
     { title: "Painel de Controle", pageName: "NetworkOverview" },
     { title: "Gerenciar Senhas", pageName: "AdminUsers" },
