9ec4f48050a7b7f9cb2a515c95c175ccc97f0abc
LUIZ ALBERTO SANTANNA FILHO <luizsantanna@tttcorporate.com>
2026-01-22 13:48:33 +0000

    File changes

--- src/pages/CatalogCheckout.jsx
+++ src/pages/CatalogCheckout.jsx
@@ -8,6 +8,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Loader2, ArrowLeft, ShoppingCart } from "lucide-react";
 import { toast } from "sonner";
+import { createMPPreference } from "@/functions/createMPPreference";
 
 const Product = base44.entities.Product;
 const CatalogSale = base44.entities.CatalogSale;
